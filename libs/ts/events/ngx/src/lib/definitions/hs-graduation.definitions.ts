af1234ba4e5c3c27746208e08e0df072821c35f3
Slone Early <sloneearly@tamu.edu>
2026-05-11 16:49:01 +0000

    working popups

--- libs/ts/events/ngx/src/lib/definitions/hs-graduation.definitions.ts
+++ libs/ts/events/ngx/src/lib/definitions/hs-graduation.definitions.ts
@@ -8,103 +8,159 @@ import {
   EventConfiguration,
   SpecialEventOptions
 } from '../interfaces/special-event.interface';
-import { commonSymbols } from './common.definitions';
 
 export enum HS_GRADUATION_LAYERS {
-  ROUTES = 'hs-graduation-routes',
-  PARKING_LOTS = 'hs-graduation-parking-lots',
-  TRAFFIC_ADVISORIES = 'hs-graduation-traffic-advisories'
+  ACCESSIBLE_BUS_PARKING = 'hs-graduation-accessible-bus-parking',
+  ARRIVAL_ROUTES = 'hs-graduation-arrival-routes',
+  ARRIVAL_PARKING = 'hs-graduation-arrival-parking',
+  DEPARTURE_ROUTES = 'hs-graduation-departure-routes',
+  TRAFFIC_ADVISORIES = 'hs-graduation-traffic-advisories',
+  DEPARTURE_PARKING = 'hs-graduation-departure-parking'
 }
 
 const eventUrl = Connections.hsGraduationUrl;
 
 const HsGraduationEventDefinitions = {
-  GRADUATION_TRAFFIC_FLOW: {
-    id: HS_GRADUATION_LAYERS.ROUTES,
-    layerId: HS_GRADUATION_LAYERS.ROUTES,
-    name: 'High School Graduation Routes',
-    url: `${eventUrl}/0`
-  },
-  GRADUATION_PARKING_LOTS: {
-    id: HS_GRADUATION_LAYERS.PARKING_LOTS,
-    layerId: HS_GRADUATION_LAYERS.PARKING_LOTS,
-    name: 'High School Graduation Parking',
+  ACCESSIBLE_BUS_PARKING: {
+    id: HS_GRADUATION_LAYERS.ACCESSIBLE_BUS_PARKING,
+    name: 'Accessible/Bus Parking',
     url: `${eventUrl}/1`
   },
-  GRADUATION_TRAFFIC_ADVISORIES: {
-    id: HS_GRADUATION_LAYERS.TRAFFIC_ADVISORIES,
-    layerId: HS_GRADUATION_LAYERS.TRAFFIC_ADVISORIES,
-    name: 'Traffic Advisories',
+  ARRIVAL_ROUTES: {
+    id: HS_GRADUATION_LAYERS.ARRIVAL_ROUTES,
+    name: 'Arrival Recommended Routes',
     url: `${eventUrl}/2`
+  },
+  ARRIVAL_PARKING: {
+    id: HS_GRADUATION_LAYERS.ARRIVAL_PARKING,
+    name: 'High School Graduation Parking',
+    url: `${eventUrl}/3`
+  },
+  DEPARTURE_ROUTES: {
+    id: HS_GRADUATION_LAYERS.DEPARTURE_ROUTES,
+    name: 'Departure Recommended Routes',
+    url: `${eventUrl}/5`
+  },
+  TRAFFIC_ADVISORIES: {
+    id: HS_GRADUATION_LAYERS.TRAFFIC_ADVISORIES,
+    name: 'Departure Traffic Advisories',
+    url: `${eventUrl}/6`
+  },
+  DEPARTURE_PARKING: {
+    id: HS_GRADUATION_LAYERS.DEPARTURE_PARKING,
+    name: 'Parking/Closures',
+    url: `${eventUrl}/7`
   }
+};
+
+const HsGraduationLayerReferences: Record<string, string> = {
+  ARRIVAL_PARKING: HS_GRADUATION_LAYERS.ARRIVAL_PARKING,
+  DEPARTURE_PARKING: HS_GRADUATION_LAYERS.DEPARTURE_PARKING,
+  ACCESSIBLE_BUS_PARKING: HS_GRADUATION_LAYERS.ACCESSIBLE_BUS_PARKING,
+  ARRIVAL_ROUTES: HS_GRADUATION_LAYERS.ARRIVAL_ROUTES,
+  DEPARTURE_ROUTES: HS_GRADUATION_LAYERS.DEPARTURE_ROUTES,
+  TRAFFIC_ADVISORIES: HS_GRADUATION_LAYERS.TRAFFIC_ADVISORIES
 };
 
 export const HsGraduationColdLayerSources: LayerSource[] = [
   {
     type: 'feature',
-    id: HsGraduationEventDefinitions.GRADUATION_TRAFFIC_FLOW.id,
-    title: HsGraduationEventDefinitions.GRADUATION_TRAFFIC_FLOW.name,
-    url: HsGraduationEventDefinitions.GRADUATION_TRAFFIC_FLOW.url,
-    popupComponent: MarkdownPopupComponent,
-    popupData: {
-      name: HsGraduationEventDefinitions.GRADUATION_TRAFFIC_FLOW.name,
-      description: 'attributes.name'
-    },
-    visible: true,
-    listMode: 'show',
-    native: {
-      outFields: ['*'],
-      renderer: {
-        type: 'unique-value',
-        field: 'name',
-        uniqueValueInfos: [
-          {
-            value: 'Departing - Recommended Route',
-            label: 'Fast Route',
-            symbol: commonSymbols.GREEN_ARROW
-          },
-          {
-            value: 'Fast Route',
-            label: 'Fast Route',
-            symbol: commonSymbols.GREEN_ARROW
-          },
-          {
-            value: 'Expect Delays',
-            label: 'Expect Delays',
-            symbol: commonSymbols.RED_ARROW
-          }
-        ]
-      }
-    }
-  },
-  {
-    type: 'feature',
-    id: HsGraduationEventDefinitions.GRADUATION_PARKING_LOTS.id,
-    title: HsGraduationEventDefinitions.GRADUATION_PARKING_LOTS.name,
-    url: HsGraduationEventDefinitions.GRADUATION_PARKING_LOTS.url,
+    id: HsGraduationEventDefinitions.ARRIVAL_PARKING.id,
+    title: HsGraduationEventDefinitions.ARRIVAL_PARKING.name,
+    url: HsGraduationEventDefinitions.ARRIVAL_PARKING.url,
     popupComponent: MarkdownWDirectionsPopupComponent,
     popupData: {
-      name: 'attributes.Type',
-      description: 'attributes.description'
+      name: { field: 'Type' },
+      description: { field: 'description' }
     },
     visible: true,
     listMode: 'show',
+    layerIndex: 60,
     native: {
       outFields: ['*']
     }
   },
   {
     type: 'feature',
-    id: HsGraduationEventDefinitions.GRADUATION_TRAFFIC_ADVISORIES.id,
-    title: HsGraduationEventDefinitions.GRADUATION_TRAFFIC_ADVISORIES.name,
-    url: HsGraduationEventDefinitions.GRADUATION_TRAFFIC_ADVISORIES.url,
+    id: HsGraduationEventDefinitions.DEPARTURE_PARKING.id,
+    title: HsGraduationEventDefinitions.DEPARTURE_PARKING.name,
+    url: HsGraduationEventDefinitions.DEPARTURE_PARKING.url,
     popupComponent: MarkdownWDirectionsPopupComponent,
     popupData: {
-      name: HsGraduationEventDefinitions.GRADUATION_TRAFFIC_ADVISORIES.name,
-      description: 'attributes.name'
+      name: { field: 'Type' },
+      description: { field: 'description' }
     },
     visible: true,
     listMode: 'show',
+    layerIndex: 61,
+    native: {
+      outFields: ['*']
+    }
+  },
+  {
+    type: 'feature',
+    id: HsGraduationEventDefinitions.ACCESSIBLE_BUS_PARKING.id,
+    title: HsGraduationEventDefinitions.ACCESSIBLE_BUS_PARKING.name,
+    url: HsGraduationEventDefinitions.ACCESSIBLE_BUS_PARKING.url,
+    popupComponent: MarkdownWDirectionsPopupComponent,
+    popupData: {
+      name: { field: 'name' },
+      description: { field: 'description' }
+    },
+    visible: true,
+    listMode: 'show',
+    layerIndex: 62,
+    native: {
+      outFields: ['*']
+    }
+  },
+  {
+    type: 'feature',
+    id: HsGraduationEventDefinitions.ARRIVAL_ROUTES.id,
+    title: HsGraduationEventDefinitions.ARRIVAL_ROUTES.name,
+    url: HsGraduationEventDefinitions.ARRIVAL_ROUTES.url,
+    popupComponent: MarkdownPopupComponent,
+    popupData: {
+      name: { field: 'name' },
+      description: { field: 'description' }
+    },
+    visible: true,
+    listMode: 'show',
+    layerIndex: 63,
+    native: {
+      outFields: ['*']
+    }
+  },
+  {
+    type: 'feature',
+    id: HsGraduationEventDefinitions.DEPARTURE_ROUTES.id,
+    title: HsGraduationEventDefinitions.DEPARTURE_ROUTES.name,
+    url: HsGraduationEventDefinitions.DEPARTURE_ROUTES.url,
+    popupComponent: MarkdownPopupComponent,
+    popupData: {
+      name: { field: 'Notes' },
+      description: { field: 'description' }
+    },
+    visible: true,
+    listMode: 'show',
+    layerIndex: 64,
+    native: {
+      outFields: ['*']
+    }
+  },
+  {
+    type: 'feature',
+    id: HsGraduationEventDefinitions.TRAFFIC_ADVISORIES.id,
+    title: HsGraduationEventDefinitions.TRAFFIC_ADVISORIES.name,
+    url: HsGraduationEventDefinitions.TRAFFIC_ADVISORIES.url,
+    popupComponent: MarkdownWDirectionsPopupComponent,
+    popupData: {
+      name: { field: 'name' },
+      description: { field: 'description' }
+    },
+    visible: true,
+    listMode: 'show',
+    layerIndex: 65,
     native: {
       outFields: ['*']
     }
@@ -112,96 +168,25 @@ export const HsGraduationColdLayerSources: LayerSource[] = [
 ];
 
 export const HsGraduationConfiguration: EventConfiguration = {
-  id: 'hs-graduation-2025',
+  id: 'hs-graduation-2026',
   name: 'High School Graduation',
   applicationName: 'High School Graduation Transportation Map',
   shortApplicationName: 'High School Graduation Map',
   introductionText: 'Get the best transportation and parking information for the high school graduation ceremonies.',
-  eventDates: ['2025-05-24', '2025-05-25'],
+  eventDates: ['2026-05-22', '2026-05-23'],
   mapCenter: [-96.34458, 30.60629],
-  zoom: 16
+  zoom: 16,
+  legendAllowVisibilityToggle: true
 };
 
-enum HsGraduationEventChoices {
-  Arrival = 'arrival',
-  Departure = 'departure'
-}
-
-export const HsGraduationOptions: SpecialEventOptions = [
-  {
-    value: 'type',
-    description:
-      'Please select the graduation map type to provide the most accurate transportation and parking information.',
-    shortDescription: 'Type',
-    label: 'Type',
-    choices: [
-      {
-        value: HsGraduationEventChoices.Arrival,
-        label: 'Arrival'
-      },
-      {
-        value: HsGraduationEventChoices.Departure,
-        label: 'Departure'
-      }
-    ],
-    effects: {
-      layers: [
-        {
-          layerId: HS_GRADUATION_LAYERS.ROUTES,
-          field: 'name',
-          conversions: [
-            {
-              input: HsGraduationEventChoices.Arrival,
-              expression: "name = 'Fast Route' OR name = 'Expect Delays'"
-            },
-            {
-              input: HsGraduationEventChoices.Departure,
-              output: 'Departing - Recommended Route'
-            }
-          ]
-        },
-        {
-          layerId: HS_GRADUATION_LAYERS.PARKING_LOTS,
-          conversions: [
-            {
-              input: HsGraduationEventChoices.Arrival,
-              expression: "Type NOT IN ('Departure - Closure')"
-            },
-            {
-              input: HsGraduationEventChoices.Departure,
-              expression: "Type NOT IN ('Closure')"
-            }
-          ]
-        },
-        {
-          layerId: HS_GRADUATION_LAYERS.TRAFFIC_ADVISORIES,
-          conversions: [
-            {
-              input: HsGraduationEventChoices.Arrival,
-              propOverrides: {
-                visible: false,
-                listMode: 'hide'
-              }
-            },
-            {
-              input: HsGraduationEventChoices.Departure,
-              propOverrides: {
-                visible: true
-              }
-            }
-          ]
-        }
-      ]
-    }
-  }
-];
+export const HsGraduationOptions: SpecialEventOptions = [];
 
 export const HsGraduationTs: AggiemapCustomMapConfiguration = {
   type: 'special-event',
   configuration: HsGraduationConfiguration,
   sources: HsGraduationColdLayerSources,
   options: HsGraduationOptions,
-  references: HS_GRADUATION_LAYERS,
+  references: HsGraduationLayerReferences,
   discover: {
     id: HsGraduationConfiguration.id,
     name: HsGraduationConfiguration.name,
